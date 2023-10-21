1e3949266ac44de7b0685a02e8523d106444c8ae
Rohan Vaidya <rohanv.rvaidya@gmail.com>
2023-10-21 12:21:09 +0530

    railway backend

--- script.js
+++ script.js
@@ -7,14 +7,17 @@ let currentQuestion = "";
 // }
 
 function saveAnswer() {
-  const answer = document.getElementById("answerInput").value;
-  //   fetch("https://quiz-app-production-1fb9.up.railway.app/saveAnswer", {
+    var hide = document.getElementById("audience-screen");
+    hide.classList.add("hide");
+  const ans = document.getElementById("answerInput").value;
+  console.log(ans)
   fetch("https://quiz-app-production-1fb9.up.railway.app/saveAnswer", {
+  // fetch("http://localhost/saveAnswer", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ answer }),
+    body: JSON.stringify({ ans }),
   })
     .then((response) => response.json())
     .then((data) => {
@@ -29,8 +32,7 @@ function saveAnswer() {
 function displayPopup() {
   var popup = document.getElementById("popup");
   popup.classList.add("show");
-  var hide = document.getElementById("audience-screen");
-  hide.classList.add("hide");
+
   // setTimeout(function () {
   //   popup.classList.remove("show");
   // }, 2000); // Adjust the time here (in milliseconds) for how long you want the popup to be displayed
